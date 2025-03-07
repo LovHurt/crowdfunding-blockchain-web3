0ab239069e9447d88e0d092fc1cb370035d4f214
lovhurt <alicandagidir@hotmail.com>
2025-03-07 17:18:39 +0300

    add routing and layout components, tailwind config

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import App from "./App";
+import "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <ThirdwebProvider chainId={ChainId.Sepolia}>
     <Router>
       <App />
---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+// tailwind.config.js
+
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+      ],
+      theme: {
+        extend: {
+          fontFamily: {
+            epilogue: ['Epilogue', 'sans-serif'],
+          },
+          boxShadow: {
+            secondary: '10px 10px 20px rgba(2, 2, 2, 0.25)',
+          },
+        },
+      },
+      plugins: [],
+    }
+  
