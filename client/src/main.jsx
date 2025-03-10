a5474368b551d5ea986260976a0df387941164a2
lovhurt <alicandagidir@hotmail.com>
2025-03-10 12:02:23 +0300

    add custom components for UI, including buttons, loaders, and campaign display; update Home and App structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import App from "./App";
-import "./index.css"
+import "./index.css";
+import { StateContextProvider } from "./context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThirdwebProvider chainId={ChainId.Sepolia}>
     <Router>
-      <App />
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
     </Router>
   </ThirdwebProvider>
 );
